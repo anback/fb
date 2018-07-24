681dced71a2e131dd3a4a16c03b1b7d2bb649d7c
anback <anders.back@me.com>
2018-07-24 07:08:11 +0200

    initial commit

---
+++ bst.js
@@ -0,0 +1,121 @@
+function Node(val){
+  this.value = val;
+  this.left = null;
+  this.right = null;
+}
+
+
+function BinarySearchTree(){
+  this.root = null;
+}
+
+BinarySearchTree.prototype.push = function(val){
+   var root = this.root;
+
+   if(!root){
+      this.root = new Node(val);
+      return;
+   }
+
+   var currentNode = root;
+   var newNode = new Node(val);
+
+   while(currentNode){
+      if(val < currentNode.value){
+          if(!currentNode.left){
+             currentNode.left = newNode;
+             break;
+          }
+          else{
+             currentNode = currentNode.left;
+        }
+     }
+     else{
+         if(!currentNode.right){
+            currentNode.right = newNode;
+            break;
+         }
+         else{
+            currentNode = currentNode.right;
+         }
+     }
+  }
+}
+
+function dfs(node){
+  if(!node) return
+  console.log(node.value);
+  dfs(node.left);
+  dfs(node.right);
+}
+
+function dfs(node){
+  if(!node) return
+  console.log(node.value);
+  dfs(node.left);
+  dfs(node.right);
+}
+
+function postorder(node){
+  if(!node) return
+  postorder(node.left);
+  postorder(node.right);
+  console.log(node.value);
+}
+
+function bfs(startNode) {
+  let arr = []
+  arr.push(startNode)
+  while(arr.length > 0) {
+    let node = arr.shift()
+    if(node.left) arr.push(node.left)
+    if(node.right) arr.push(node.right)
+
+    console.log(node.value)
+  }
+}
+
+let min = (node) => {
+  if(!!node && !!node.left) return min(node.left)
+  return node.value
+}
+
+let max = (node) => {
+  if(!!node && !!node.right) return max(node.right)
+  return node.value
+}
+
+let isBST = (node) => {
+  if(!node) return true
+
+  if(!!node.right && node.right.value < node.value) return false
+  if(!!node.left && node.left.value > node.value) return false
+
+  return isBST(node.left) && isBST(node.right)
+}
+
+let maxValueOfPathSum = (node) => {
+  if(!node) return 0
+
+  return node.value + Math.max(maxValueOfPathSum(node.left), maxValueOfPathSum(node.right))
+}
+
+
+var bst = new BinarySearchTree();
+bst.push(2);
+bst.push(1);
+bst.push(3);
+bst.push(1);
+bst.push(1);
+bst.push(1);
+bst.push(1);
+bst.push(1);
+/*
+    3
+  2   5
+ 1   4
+ */
+
+console.log(JSON.stringify(bst.root, undefined, 2))
+
+console.log('maxValueOfPathSum', maxValueOfPathSum(bst.root))
